84eb3459e12b57f92ac529c37c905c3df2723a0d
lza_menace <lza_menace@protonmail.com>
2021-09-01 10:15:54 -0700

    add script for specifying metadata uri

---
+++ scripts/setBaseURI.js
@@ -0,0 +1,19 @@
+const newURI = '';
+
+module.exports = async function main(callback) {
+  try {
+    const NonFungibleSoup = artifacts.require("NonFungibleSoup");
+    const nfs = await NonFungibleSoup.deployed();
+    if (newURI == '') {
+      console.log('You need to specify a metadata URI where assets can be loaded. ie: "ipfs://xxxxxx/"');
+      callback(1);
+    } else {
+      await nfs.setBaseURI(newURI);
+      console.log(`Set new contract base metadata URI as: ${newURI}`);
+      callback(0);
+    }
+  } catch (error) {
+    console.error(error);
+    callback(1);
+  }
+}
